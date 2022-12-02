7b760cd85febdb2b3a050c7e903e10f1e791dd75
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-12-02 05:13:19 +0300

    Completed Challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+   
+  let inputBox = document.getElementById('create-task-form')
+  // console.log(inputBox)
+  
+
+  inputBox.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.newtask.value)
+    inputBox.reset()
+    
+  } )
+  
+})
+
+function handleToDo(list) {
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x'
+  p.textContent = list
+  p.appendChild(btn)
+  
+  document.querySelector('#tasks').appendChild(p)
+}
+function handleDelete(e) {
+  e.target.parentNode.remove()
+
+}
+
+// let p = document.getElementById('tasks')
+//     let li = p.createElement("li")
+//     let btn = p.createElement("button")
+//     btn.textContent = "x"
+//     li.textContent = `${cont}`
+//     btn.addEventListener("click", (e){
+//       p.removeChild()
+//     }) 
